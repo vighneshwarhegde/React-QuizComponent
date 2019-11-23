c92432a92257030b0bc5fb11618a20e5e5b73e06
Vighneshwar Hegde <v.hegde@sap.com>
2019-11-23 16:19:33 +0530

    added quiz.js1

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
+let quizData = require("./quiz_data.json");
 
-let quizData = require('./quiz_data.json');
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {quiz_poistion:1};
+  }
 
-class Quiz extends Component{
-
-    constructor(props){
-        super(props)
-        this.state = { quiz_poistion: 1 }
-    }
-
-    render(){
-    return <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
-    }
-
+  render() {
+    return (
+      <div>
+        <div className="QuizQuestion">
+          {quizData.quiz_questions[0].instruction_text}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Quiz
+export default Quiz;
